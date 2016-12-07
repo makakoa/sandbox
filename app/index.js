8b7d15dd8376971430f24ee20e8a3b1f57a8cb41
Cameron Yee <makakoa57@gmail.com>
2016-12-07 13:41:43 -0800

    trying out aframe with react

--- app/index.js
+++ app/index.js
@@ -1,13 +1,48 @@
 'use strict';
 
 var ReactDOM = require('react-dom'),
+    aframe = require('aframe-react'),
+    Scene = aframe.Scene,
+    Entity = aframe.Entity,
     rust = require('rust');
 
+require('aframe');
+
 ReactDOM.render(
-  rust.element(
+  rust.o2([
     'div',
-    null,
-    'Hello World'
-  ),
+    // 'Hello World',
+
+    [
+      Scene,
+
+      ['a-sky', {
+        color: '#444444'
+      }],
+
+
+      [Entity, {
+        geometry: {
+          primitive: 'box',
+          color: '#4279ff',
+          width: '4',
+          height: '10',
+          depth: '2'
+        }
+      }]
+    ]
+
+    // ['a-scene',
+
+    //   ['a-sky', {
+    //     color: '#4279ff'
+    //   }],
+    //   ['a-light', {
+    //     type: 'point',
+    //     color: 'blue',
+    //     position: '0 5 0'
+    //   }]
+    // ]
+  ]),
   document.getElementById('app-entry')
 );
